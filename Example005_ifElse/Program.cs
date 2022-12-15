ad750c597685a08ed85846c4acb59859be844664
Alexander_Kosilov <aikos3@yandex.ru>
2022-12-15 18:13:15 +0500

    add Example005_ConditionIfElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+    if (username.ToLower() == "маша")
+    {
+       Console.WriteLine("Ура! Привет, Маша :)");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }

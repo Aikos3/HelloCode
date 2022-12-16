5432e2bd858aa13c2f6e33e2e63b557185801b0e
Alexander_Kosilov <aikos3@yandex.ru>
2022-12-16 18:17:00 +0500

    Remake program

---
+++ MyHomeWork/Example_Kvadrat/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a = ConvertToConsole.Read();
+Console.WriteLine("Введите второе число");
+int b = Console.Read();
+if (a == b * b)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}

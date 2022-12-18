21d5b9fda78fd7d6134cfa2c08cd76dd33bfd545
Alexander_Kosilov <aikos3@yandex.ru>
2022-12-19 00:59:06 +0500

    add HW Lesson1_Example4

---
+++ MyHomeWork/C#_Lesson1_example4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int maxNum = firstNumber;
+if (secondNumber > maxNum) maxNum = secondNumber;
+if (thirdNumber > maxNum) maxNum = thirdNumber;
+    
+Console.WriteLine($"Максимальное из трёх чисел: {maxNum}");

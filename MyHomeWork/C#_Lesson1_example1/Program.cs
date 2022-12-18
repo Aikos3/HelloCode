756c1fcdc9647995c4f12266c0b85d0b2179b1e0
Alexander_Kosilov <aikos3@yandex.ru>
2022-12-19 01:17:28 +0500

    Add C#_Lesson1_example1

---
+++ MyHomeWork/C#_Lesson1_example1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Программа, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+//Console.WriteLine("Введите число");
+//int number = Convert.ToInt32(Console.ReadLine()); //Преобразовываем введенное значение в целочисленное значение с помощью оператора Convert.ToInt32
+//int square = number * number;
+//Console.WriteLine("Квадрат числа "+ number + " будет " + square);
+
+// Более сложный пример, где производится предварительная проверка числа (целое, без букв и тд.)
+Console.WriteLine("Введите число");
+string stringNumber = Console.ReadLine();
+int number;
+while (!int.TryParse (stringNumber, out number))
+{
+    Console.WriteLine("Ошибка! Введите целое число");
+    stringNumber = Console.ReadLine().Trim();
+}
+int result = number * number;
+Console.WriteLine($"квадрат числа {number} равен {result}"); //Добавили $ - знак интерполяции 
+

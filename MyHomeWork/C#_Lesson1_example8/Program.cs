b46d2cd0cc16bbaed6cfdeef9e87b96a01219c47
Alexander_Kosilov <aikos3@yandex.ru>
2022-12-20 23:49:27 +0500

    Add C#_Lesson1_example8

--- MyHomeWork/C#_Lesson1_example8/Program.cs
+++ MyHomeWork/C#_Lesson1_example8/Program.cs
@@ -1,1 +1,19 @@
-﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N./
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число:");
+    
+int numberN = Convert.ToInt32(Console.ReadLine());
+    int element = 2;
+
+    while (element <= numberN) 
+    {
+      Console.WriteLine(element);
+      element = element + 2;
+    };    
+    
+    
+  

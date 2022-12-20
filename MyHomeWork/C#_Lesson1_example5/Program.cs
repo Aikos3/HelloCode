50a9da149cd21118f0ee629f7747a026c631b1f0
Alexander_Kosilov <aikos3@yandex.ru>
2022-12-20 23:56:39 +0500

    Add C#_Lesson1_example5

--- MyHomeWork/C#_Lesson1_example5/Program.cs
+++ MyHomeWork/C#_Lesson1_example5/Program.cs
@@ -1,6 +1,15 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N), 
 // а на выходе показывает все целые числа в промежутке от -N до N.
 
-int numberN;
+Console.WriteLine("Введите число:");
+    
+int numberN = Convert.ToInt32(Console.ReadLine());
+    int element = -numberN;
+
+    while (element <= numberN) 
+    {
+      Console.WriteLine(element);
+      element++;
+    };    
 
 

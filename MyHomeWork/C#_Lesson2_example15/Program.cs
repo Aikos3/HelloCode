d10008b9b646e5af60f3eba666cc4f9f548f5f01
Alexander_Kosilov <aikos3@yandex.ru>
2022-12-25 22:18:31 +0500

    Add C#_Lesson2_example10

---
+++ MyHomeWork/C#_Lesson2_example15/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine($"Третьей цифры нет");
+    return;
+}
+int thirdNumber = (num / 100) % 10;
+Console.WriteLine($"Третья цифра числа {num} -> {thirdNumber}");

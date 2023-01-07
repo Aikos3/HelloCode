1197ff4224e7327bc279a7ce0c40beca4a8f1731
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-07 23:00:28 +0500

    clear

--- MyHomeWork/C#_Lesson3_example17/Program.cs
+++ MyHomeWork/C#_Lesson3_example17/Program.cs
@@ -2,11 +2,8 @@
 // Напишите программу, которая принимает на вход координаты точки (X и Y), 
 // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-Console.WriteLine("Введите координату X:");
-int xPoint = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату Y:");
-int yPoint = Convert.ToInt32(Console.ReadLine());
-
+void Show (int xPoint, int yPoint)
+{
 if (xPoint == 0 || yPoint == 0){
 Console.WriteLine("Невозможно определить четверть");
 }
@@ -20,5 +17,14 @@ else if (xPoint < 0 && yPoint < 0){
 Console.WriteLine("Точка находится во 3-й четверти");
 }
 else if (xPoint > 0 && yPoint < 0){
-Console.WriteLine("Точка находится во 2-й четверти");
+Console.WriteLine("Точка находится во 4-й четверти");
 };
+};
+
+Console.WriteLine("Введите координату X:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Show (5,12);
+Show (x, y);

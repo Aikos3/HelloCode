a88aecc4d2e6ee24e1b9c195c334304eac4b0b72
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-17 00:09:19 +0500

    add ex 29

--- MyHomeWork/C#_Lesson4_example29/Program.cs
+++ MyHomeWork/C#_Lesson4_example29/Program.cs
@@ -2,3 +2,17 @@
 // и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+int[] array = GetBinaryArray(8);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 100);
+    }
+
+    return result;
+}

ea10ab84a183ce2a13594ba5190355675638fde9
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-17 00:21:44 +0500

    edit ex 25

--- MyHomeWork/C#_Lesson4_example25/Program.cs
+++ MyHomeWork/C#_Lesson4_example25/Program.cs
@@ -3,13 +3,34 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+// Console.WriteLine("Введите первое число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Второе число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int result = a;
+// for (int i = 1; i < b; i++)
+// {
+//     result = result * a;
+// }
+// Console.WriteLine($"{a} в степени {b} = {result}");
+
+
+//ниже добавил функцию
+
+
+int GetDegree(int num1, int num2)
+{
+    int result = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+
 Console.WriteLine("Введите первое число:");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите Второе число:");
 int b = Convert.ToInt32(Console.ReadLine());
-int result = a;
-for (int i = 1; i < b; i++)
-{
-    result = result * a;
-}
-Console.WriteLine($"{a} в степени {b} = {result}");
+
+Console.WriteLine($"{a} в степени {b} = {GetDegree(a, b)}");

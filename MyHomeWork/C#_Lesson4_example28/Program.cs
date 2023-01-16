96508f3e778a61c6f5b2d000a9eef1051fda862e
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-16 23:05:06 +0500

    add ex 28

---
+++ MyHomeWork/C#_Lesson4_example28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int GetMultiNum(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
+;
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {number} = {GetMultiNum(number)}");

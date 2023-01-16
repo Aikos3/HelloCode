9404ed87655d9dc6125c67b404eb8d29c2c384f5
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-16 19:49:51 +0500

    add ex 26

--- MyHomeWork/C#_Lesson4_example26/Program.cs
+++ MyHomeWork/C#_Lesson4_example26/Program.cs
@@ -1,3 +1,16 @@
-﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт 
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт
 // количество цифр в числе.
 
+int GetDigitCount(int num)
+{
+int digitCount = 1;
+while (num >= 10)
+{
+    num = num / 10;
+    digitCount++;
+}
+return digitCount;
+}
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"В данном числе {GetDigitCount(number)} цифр");

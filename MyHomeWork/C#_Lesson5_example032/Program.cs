16e9c4f66af128818c2a99ecc074e35d8dd71166
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-17 17:09:09 +0500

    add ex32

---
+++ MyHomeWork/C#_Lesson5_example032/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 32: Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] array = GetArray(10);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+array = InvertArray(array);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(-100, 100);
+    }
+
+    return result;
+}
+
+int[] InvertArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}

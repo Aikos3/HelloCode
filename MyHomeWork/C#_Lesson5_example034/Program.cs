ca9e39ededc0141492ec5e2c532282b0ef7f7c24
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-18 13:25:48 +0500

    add ex 34

---
+++ MyHomeWork/C#_Lesson5_example034/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 
+
+int[] array = GetArray(8);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 999);
+    }
+
+    return result;
+}
+
+int elementCount = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        elementCount++;
+    }
+}
+ Console.WriteLine($"Количество чётных элементов в массиве = {elementCount}");

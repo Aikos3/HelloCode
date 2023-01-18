880d8bb798efc94052d481bf80f65a52a109f598
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-18 15:42:11 +0500

    add ex 36

---
+++ MyHomeWork/C#_Lesson5_example036/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = GetArray(10);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 10);
+    }
+    return result;
+}
+
+int sumElement = 0;
+
+for (int count = 1; count < array.Length; count = count + 2)
+{
+    sumElement += array[count];
+}
+
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {sumElement}");

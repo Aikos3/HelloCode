9a706e9288b5d514ddaf55f28757a96375e92616
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-20 12:36:14 +0500

    adit ex 38

--- MyHomeWork/C#_Lesson5_example038/Program.cs
+++ MyHomeWork/C#_Lesson5_example038/Program.cs
@@ -1,36 +1,61 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между
 // максимальным и минимальным элементом массива.
 
-// double[] array = new double[10];
+//Пример решения с массивом из простых чисел
+
+// Console.Clear();
+// int[] array = GetArray(10);
 // Console.WriteLine($" [ {String.Join(", ", array)} ] ");
 
-// double[] GetArray(float size)
+// int[] GetArray(int size)
 // {
-//     double[] result = new double[size];
-//     for (float  i = 0; i < size; i++)
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i++)
 //     {
-//         result[i] = new Random().NextDouble(1.0, 10.0f);
+//         result[i] = new Random().Next(-99, 100);
 //     }
+
 //     return result;
 // }
+// int maxElement = -99;
+// int minElement = 99;
+// int result = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > maxElement)
+//         maxElement = array[i];
+// }
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] < minElement)
+//         minElement = array[i];
+// }
+// result = maxElement - minElement;
+
+// Console.WriteLine($"Максимальное значение элемента: {maxElement}, минимальное значение элемента: {minElement}");
+// Console.WriteLine($"Разница между максимальным и минимальным значением: {result}");
+
+
+//Пример решения с массивом из вещественных чисел
 
 Console.Clear();
-int[] array = GetArray(10);
-Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+double[] array = GetArray(10, -99, 99);
+Console.WriteLine($" [ {String.Join(" ", array)} ] ");
 
-int[] GetArray(int size)
+double[] GetArray(int size, int min, int max)
 {
-    int[] result = new int[size];
+    double[] result = new double[size];
+    Random rnd = new Random();
     for (int i = 0; i < size; i++)
-    {
-        result[i] = new Random().Next(-99, 100);
-    }
+        result[i] = Math.Round(rnd.NextDouble() + rnd.Next(min, max), 2);
 
     return result;
 }
-int maxElement = -99;
-int minElement = 99;
-int result = 0;
+
+double maxElement = array[0];
+double minElement = array[0];
+double result = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
@@ -44,5 +69,7 @@ for (int i = 0; i < array.Length; i++)
 }
 result = maxElement - minElement;
 
-Console.WriteLine($"Максимальное значение элемента: {maxElement}, минимальное значение элемента: {minElement}");
+Console.WriteLine(
+    $"Максимальное значение элемента: {maxElement}, минимальное значение элемента: {minElement}"
+);
 Console.WriteLine($"Разница между максимальным и минимальным значением: {result}");

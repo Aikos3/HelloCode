974a177e85db43de7b4bcb16ae84bf663843960d
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-20 12:15:04 +0500

    edit ex 34

--- MyHomeWork/C#_Lesson5_example034/Program.cs
+++ MyHomeWork/C#_Lesson5_example034/Program.cs
@@ -10,7 +10,7 @@ int[] GetArray(int size)
     int[] result = new int[size];
     for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(0, 999);
+        result[i] = new Random().Next(100, 999);
     }
 
     return result;

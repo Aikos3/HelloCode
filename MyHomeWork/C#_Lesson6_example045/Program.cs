c712672f6f2a2fdb2a64a0c06b21d74d7b82d024
Alexander_Kosilov <aikos3@yandex.ru>
2023-01-24 15:30:05 +0500

    add ex 45

---
+++ MyHomeWork/C#_Lesson6_example045/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования
+
+int[] arr = CreateRandomArray(10, 10, 100);
+Console.WriteLine($"[{String.Join(", ", arr)}]");
+int[] array = CopyArray(arr);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] CopyArray(int[] array)
+{
+    int[] copArr = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copArr[i] = array[i];
+    }
+    return copArr;
+}
+
+int[] CreateRandomArray(int size, int start, int end)
+{
+int[] RandomArray = new int[size];
+Random random = new Random();
+for (int i = 0; i < size; i++)
+{
+    RandomArray[i] = random.Next(start, end);
+}
+return RandomArray;
+}

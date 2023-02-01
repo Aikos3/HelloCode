6ab4666d2dd898f0af800611cc66c159f92448f5
Alexander Kosilov <119531361+Aikos3@users.noreply.github.com>
2023-02-01 23:44:27 +0500

    Update Program.cs

--- MyHomeWork/C#_Lesson7_example047/Program.cs
+++ MyHomeWork/C#_Lesson7_example047/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными
 // числами.
 // m = 3, n = 4.
 // 0,5 7 -2 -0,2
@@ -7,12 +7,14 @@
 
 double[,] myArray = new double[3, 4];
 Random random = new Random();
+double minValue = -10;
+double maxValue = 10;
 
 for (int y = 0; y < myArray.GetLength(0); y++)
 {
     for (int x = 0; x < myArray.GetLength(1); x++)
     {
-        myArray[y, x] = Math.Round(random.NextDouble(), 2);
+        myArray[y, x] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
     }
     System.Console.WriteLine();
 }

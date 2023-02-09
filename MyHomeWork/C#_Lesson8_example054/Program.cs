9aff60269dd893725c5c2b4966c4272263744c81
Alexander_Kosilov <aikos3@yandex.ru>
2023-02-09 18:45:07 +0500

    add ex54

--- MyHomeWork/C#_Lesson8_example054/Program.cs
+++ MyHomeWork/C#_Lesson8_example054/Program.cs
@@ -10,34 +10,33 @@
 // 8 4 4 2
 
 
-Console.Clear();
+Console.Clear(); // очищаем консоль, запрашиваем размеры массива 
 
 Console.Write("Укажите, сколько строк будет в массиве:");
 int rows = int.Parse(Console.ReadLine());
 Console.Write("Укажите, сколько столбцов будет в массиве:");
 int columns = int.Parse(Console.ReadLine());
 
-int[,] array = GetArray (rows, columns, 0, 10); 
+int[,] array = GetArray(rows, columns, 0, 10);
 PrintArray(array);
 SortArray(array);
-Console.WriteLine();
+Console.WriteLine("Отсортированный массив:");
 PrintArray(array);
 
-int[,] GetArray (int m, int n, int min, int max)
+int[,] GetArray(int m, int n, int min, int max) // метод, формирующий массив 
 {
     int[,] result = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result [i, j] = new Random().Next(min, max + 1);
+            result[i, j] = new Random().Next(min, max + 1);
         }
     }
     return result;
 }
 
-
-void PrintArray (int [,] inArray)
+void PrintArray(int[,] inArray) // метод, печатающий массив 
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
     {
@@ -49,22 +48,21 @@ void PrintArray (int [,] inArray)
     }
 }
 
-
-void SortArray (int [,] array)
+void SortArray(int[,] array) // метод, сортирующий массив 
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-           for (int t = j + 1; t < array.GetLength(1); t++)
-           {
-            if (array[i,t] > array[i,j])
+            for (int t = j + 1; t < array.GetLength(1); t++)
             {
-                int temp  = array[i,j];
-                array[i,j] = array[i,t];
-                array[i,t] = temp;
+                if (array[i, t] > array[i, j])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, t];
+                    array[i, t] = temp;
+                }
             }
-           } 
         }
     }
 }

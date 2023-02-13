b3913b83f3812a02661520a45edcd056707f4632
Alexander_Kosilov <aikos3@yandex.ru>
2023-02-13 18:57:16 +0500

    Написан код основной программы

---
+++ MY_CONTROL_WORK/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше либо равна 3 символа.
+
+Console.WriteLine("Введите через пробел несколько слов разной длины:");
+string inputString = Console.ReadLine();
+string[] arrayOriginal = inputString.Split(' ');
+
+string[] arrayNew = new string[arrayOriginal.Length];
+
+FindElement(arrayOriginal, arrayNew);
+Console.Write($"Длина вот этих элементов меньше или равна 3 символа --> ");
+PrintArray(arrayNew);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void FindElement(string[] arrayInput, string[] arrayOutput)
+{
+    int count = 0;
+    for (int i = 0; i < arrayInput.Length; i++)
+    {
+        if (arrayInput[i].Length <= 3)
+        {
+            arrayOutput[count] = arrayInput[i];
+            count++;
+        }
+    }
+}
